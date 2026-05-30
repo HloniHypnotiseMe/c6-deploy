c4ffe29289f8eb09fe9e0ec503baa7fa6e33dd23
VAT PRODUCTION <hloni@c6group.co.za>
2026-05-30 23:38:00 +0200

    Fix API routing

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'C6 is alive'
-
-@app.route('/control')
-def control():
-    return open('dashboard/jarvis_dashboard.html').read()
+from dashboard.auth_server import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
